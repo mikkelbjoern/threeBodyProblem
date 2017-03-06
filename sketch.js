f1e7a16a748e0087e1771a7272f079aa0b880138
Mikkel Goldschmidt <mikkelbjoerngoldschmidt@gmail.com>
2017-03-06 10:12:32 +0100

    Changed to classes and made a third body

--- sketch.js
+++ sketch.js
@@ -1,25 +1,55 @@
 function setup() {
-  createCanvas(1000,1000);
+  createCanvas(2000,1000);
 }
 
 function draw() {
   background(0);
-  ellipse(s2.x, s2.y, (m2/3)**0.5, (m2/3)**0.5);
-  ellipse(s1.x, s1.y, (m1/3)**0.5, (m1/3)**0.5);
+  ellipse(b2.s.x, b2.s.y, (b2.m/3.14)**0.5, (b2.m/3)**0.5);
+  ellipse(b1.s.x, b1.s.y, (b1.m/3.14)**0.5, (b1.m/3)**0.5);
+  ellipse(b3.s.x, b3.s.y, (b3.m/3.14)**0.5, (b3.m/3)**0.5);
 
+}
+
+function Body(s,v,a,m) {
+  this.s = s
+  this.v = v
+  this.a = a
+  this.m = m
+
+  this.nullForce = function(){
+    this.a = new p5.Vector(0,0);
+  }
+  this.addForce = function(body) {
+    this.a.x += body.m*Gk*(body.s.x - this.s.x)/Math.abs(((this.s.dist(body.s)+0.1)**3));
+    this.a.y += body.m*Gk*(body.s.y - this.s.y)/Math.abs(((this.s.dist(body.s)+0.1)**3));
+  }
+
+  this.move = function() {
+    this.v.add(this.a)
+    this.s.add(this.v)
+  }
 }
 
 let Gk = 6.62
 
-let s1 = new p5.Vector(500,600);
-let v1 = new p5.Vector(-6,0);
+let s1 = new p5.Vector(1000,950);
+let v1 = new p5.Vector(50,0);
 let a1 = new p5.Vector(0,0);
-let m1 = 300
+let m1 = 500
 
-let s2 = new p5.Vector(500,500);
+let s2 = new p5.Vector(1000,500);
 let v2 = new p5.Vector(0,0);
 let a2 = new p5.Vector(0,0);
-let m2 = 500
+let m2 = 200000
+
+let s3 = new p5.Vector(1000,50);
+let v3 = new p5.Vector(-50,0);
+let a3 = new p5.Vector(0,0);
+let m3 = 500
+
+let b1 = new Body(s1, v1, a1, m1)
+let b2 = new Body(s2, v2, a2, m2)
+let b3 = new Body(s3, v3, a3, m3)
 
 
 var loo = setInterval(loo, 50);
@@ -27,14 +57,20 @@ var loo = setInterval(loo, 50);
 
 
 function loo(){
-  a1.x = m2*Gk*(s2.x - s1.x)/Math.abs(((s1.dist(s2))**3))
-  a1.y = m2*Gk*(s2.y - s1.y)/Math.abs(((s1.dist(s2))**3))
-  console.log(v1);
-  v1 = v1.add(a1);
-  s1 = s1.add(v1);
+  b1.nullForce();
+  b2.nullForce();
+  b3.nullForce();
 
-  //a2 = 0;
-  //console.log(a2);
-  v2 = v2.add(a2);
-  s2 = s2.add(v2);
+  b1.addForce(b2);
+  b1.addForce(b3);
+  b2.addForce(b1);
+  b2.addForce(b3);
+  b3.addForce(b1);
+  b3.addForce(b2);
+
+  b1.move();
+  b2.move();
+  b3.move();
+
+
 }
