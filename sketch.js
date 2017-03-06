3d9de7ba8d690f588a914e823d77b88b0daa83cf
Mikkel Goldschmidt <mikkelbjoerngoldschmidt@gmail.com>
2017-03-06 12:40:54 +0100

    Added another set of objects

--- sketch.js
+++ sketch.js
@@ -5,6 +5,13 @@ function setup() {
 
 function draw() {
   background(0);
+
+  fill(color('white'));
+  ellipse(bB.s.x, bB.s.y, (bB.m/3.14)**0.5, (bB.m/3)**0.5);
+  ellipse(bA.s.x, b1.s.y, (bA.m/3.14)**0.5, (bA.m/3)**0.5);
+  ellipse(bC.s.x, b3.s.y, (bC.m/3.14)**0.5, (bC.m/3)**0.5);
+
+  fill(color('red'));
   ellipse(b2.s.x, b2.s.y, (b2.m/3.14)**0.5, (b2.m/3)**0.5);
   ellipse(b1.s.x, b1.s.y, (b1.m/3.14)**0.5, (b1.m/3)**0.5);
   ellipse(b3.s.x, b3.s.y, (b3.m/3.14)**0.5, (b3.m/3)**0.5);
@@ -21,8 +28,8 @@ function Body(s,v,a,m) {
     this.a = new p5.Vector(0,0);
   }
   this.addForce = function(body) {
-    this.a.x += body.m*Gk*(body.s.x - this.s.x)/Math.abs(((this.s.dist(body.s)+1)**3));
-    this.a.y += body.m*Gk*(body.s.y - this.s.y)/Math.abs(((this.s.dist(body.s)+1)**3));
+    this.a.x += body.m*Gk*(body.s.x - this.s.x)/Math.abs(((this.s.dist(body.s)+0.1)**3));
+    this.a.y += body.m*Gk*(body.s.y - this.s.y)/Math.abs(((this.s.dist(body.s)+0.1)**3));
   }
 
   this.move = function() {
@@ -33,25 +40,13 @@ function Body(s,v,a,m) {
 
 let Gk = 6.62
 
-let s1 = new p5.Vector(400,400);
-let v1 = new p5.Vector(-2,0);
-let a1 = new p5.Vector(0,0);
-let m1 = 300
+let b1 = new Body(new p5.Vector(1000,500), new p5.Vector(0,0), new p5.Vector(0,0), 40000)
+let b2 = new Body(new p5.Vector(1000,700), new p5.Vector(40,0), new p5.Vector(0,0), 300+0)
+let b3 = new Body(new p5.Vector(1000,300), new p5.Vector(-40,0), new p5.Vector(0,0), 300+0)
 
-let s2 = new p5.Vector(500,200);
-let v2 = new p5.Vector(1,-(3**0.5));
-let a2 = new p5.Vector(0,0);
-let m2 = 300
-
-let s3 = new p5.Vector(600,400);
-let v3 = new p5.Vector(1,3**0.5);
-let a3 = new p5.Vector(0,0);
-let m3 = 300
-
-let b1 = new Body(s1, v1, a1, m1)
-let b2 = new Body(s2, v2, a2, m2)
-let b3 = new Body(s3, v3, a3, m3)
-
+let bA = new Body(new p5.Vector(1000,500+0.000000), new p5.Vector(0,0), new p5.Vector(0,0), 40000)
+let bB = new Body(new p5.Vector(1000,700), new p5.Vector(40,0), new p5.Vector(0,0), 300+0)
+let bC = new Body(new p5.Vector(1000,300), new p5.Vector(-40,0), new p5.Vector(0,0), 300+0)
 
 var loo = setInterval(loo, 50);
 
@@ -72,6 +67,21 @@ function loo(){
   b1.move();
   b2.move();
   b3.move();
+
+  bA.nullForce();
+  bB.nullForce();
+  bC.nullForce();
+
+  bA.addForce(bB);
+  bA.addForce(bC);
+  bB.addForce(bA);
+  bB.addForce(bC);
+  bC.addForce(bA);
+  bC.addForce(bB);
+
+  bA.move();
+  bB.move();
+  bC.move();
 
 
 }
